cc308b2346535376951390b06441cd225c6cb214
Niki Fereidooni <nikifereidooni@gmail.com>
2021-12-10 20:29:16 -0500

    updated navbar dropdown and adjusted styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,14 @@
 import React, {useState} from "react";
 import "./style.css";
-import { Container, Navbar, Nav, Image} from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { Container, Navbar, Nav, NavDropdown, Image} from "react-bootstrap";
 
 
 function Header(props) {
 
   const [expanded, setExpanded] = useState(false);
+
+//   const navDropdownTitle = (<i class="fas fa-tachometer-alt"></i>);
+  
 
   return (
     <Container>
@@ -18,16 +20,20 @@ function Header(props) {
         fixed="top"
         expanded={expanded}
       >
-        <Navbar.Brand href="http://www.preprlabs.org" className="logo">
-        <Image src="https://d2qgo50yy98y8.cloudfront.net/public/front/img/logo.png"/>
+        <Navbar.Brand href="http://www.preprlabs.org" className="navbarBrand">
+            <Image className="logo" src="https://d2qgo50yy98y8.cloudfront.net/public/front/img/logo.png"/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto mt-1 mt-lg-0">
-            <NavLink className="navLink" to="/" onClick={() => setExpanded(false)}><img src="https://d2qgo50yy98y8.cloudfront.net/public/front/img/explore.png" alt="Explore"/>&nbsp;Explore</NavLink> &nbsp; &nbsp;
-            <NavLink className="navLink" to="/" onClick={() => setExpanded(false)}><img src="https://d2qgo50yy98y8.cloudfront.net/public/front/img/web-browser.png" alt="Dashboard" height="15px"/>&nbsp;My Dashboard</NavLink> &nbsp; &nbsp;
-            <NavLink className="navLink" to="/design" onClick={() => setExpanded(false)}><img src="https://d2qgo50yy98y8.cloudfront.net/public/front/img/settings.png" alt="Manage" height="15px"/>&nbsp;Manage</NavLink> &nbsp; &nbsp;
-            <input placeholder="Search Challenges, Labs, Projects and People"/>
+            <Nav.Link className="navLink" href="#" onClick={() => setExpanded(false)}><i class="fas fa-binoculars"></i>&nbsp;Explore</Nav.Link> 
+            <NavDropdown className="dashboardLink" title={<div style={{display: "inline-block"}}><i class="fas fa-tachometer-alt"></i> My Dashboard </div>} onClick={() => setExpanded(false)}>
+                <NavDropdown.Item href="#">User Dashboard</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown className="dashboardLink" title={<div style={{display: "inline-block"}}><i class="fas fa-cog"></i> Manage </div>} onClick={() => setExpanded(false)}>
+                <NavDropdown.Item href="#">Profile</NavDropdown.Item>
+            </NavDropdown>
+            <input className="searchBar" placeholder="Search Challenges, Labs, Projects and People"/>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
