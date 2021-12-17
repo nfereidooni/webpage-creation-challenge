099cf6679c3ecdcdf00c0aa40299fc649bb42645
Niki Fereidooni <nikifereidooni@gmail.com>
2021-12-16 19:37:27 -0500

    started working on recommended section

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,9 +7,14 @@ function Home() {
 
   return (
     <Container>
+
         <div className="banner">        
             <Image width="100%" src={banner_01} alt="Green Banner" rounded />
             <div class="centered">Welcome back, Niki Fereidooni!</div>
+        </div>
+
+        <div className="recommended row">
+            <Image src="" className="col-xs-12 col-md-6 col-lg-4" rounded />
         </div>
     </Container>
   );
