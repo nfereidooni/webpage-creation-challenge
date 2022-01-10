e97fece95ae14657baab3f96309bd7d725d72d5f
Niki Fereidooni <nikifereidooni@gmail.com>
2022-01-10 01:46:38 -0500

    added recommended section for card data

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,34 +1,38 @@
 import { React, useState } from "react";
 import "./style.css";
-import { Container, Image, CardGroup, Button} from "react-bootstrap";
+import { Container, Image, CardGroup, Button, Tabs, Tab} from "react-bootstrap";
 import banner_01 from "../assets/img/preprbanner_01.jpg"
 import OneCard from "../OneCard"
 
 function Home() {
 
-  const [key, setKey] = useState('home');
+  const [key, setKey] = useState('challenges');
 
   const CODE_DATA_CHALLENGES = [
 
     {title: "Backend Developer Challenge 1: SQL",
     image: "https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/challenge/hn6fd9vCImNkx6Eygm4cNkUnO.png",
     imageAlt: "Backend Developer Challenge icon Banner",
-    link: "https://preprlabs.org/challengeManager/backend-developer-challenge-1-sql"
+    link: "https://preprlabs.org/challengeManager/backend-developer-challenge-1-sql",
+    recommended: "yes"
   },
     {title: "Backend Developer Challenge 2: HTML, CSS and PHP",
     image: "https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/challenge/Xmjj54N0oStU9seXcLArlNgpq.png",
     imageAlt: "Backend Developer Challenge icon Banner",
-    link: "https://preprlabs.org/challengeManager/backend-developer-challenge-2-html-css-and-php"
+    link: "https://preprlabs.org/challengeManager/backend-developer-challenge-2-html-css-and-php",
+    recommended: "yes"
   },
     {title: "Backend Developer Challenge 3: Ruby On Rails",
     image: "https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/challenge/gekFqWWbKv9XP9Bjh40HoDEFM.png",
     imageAlt: "Backend Developer Challenge Banner",
-    link: "https://preprlabs.org/challengeManager/backend-developer-challenge-3-ruby-on-rails"
+    link: "https://preprlabs.org/challengeManager/backend-developer-challenge-3-ruby-on-rails",
+    recommended: "yes"
   },
     {title: "Backend Developer Challenge 4: JavaScript",
     image: "https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/challenge/ogQbXxn46Kp796X8xDnexPYJG.png",
     imageAlt: "Backend Developer Challenge Banner",
-    link: "https://preprlabs.org/challengeManager/backend-developer-challenge-4-javascript"
+    link: "https://preprlabs.org/challengeManager/backend-developer-challenge-4-javascript",
+    recommended: "yes"
   }
   ]
 
@@ -37,22 +41,26 @@ function Home() {
     {title: "FUN Lab - Winter 2021",
     image: "https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/labs/895imtbPdSuzOstEtgCSJZ7pv.jpg",
     imageAlt: "FUN Winter 2021 logo Banner",
-    link: "https://preprlabs.org/labs/fun-lab-winter-2021"
+    link: "https://preprlabs.org/labs/fun-lab-winter-2021",
+    recommended: "yes"
   },
     {title: "Web Developer Career Lab",
     image: "https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/labs/R0gpwVFsCiJdNMwLvsJRMFTwh.png",
     imageAlt: "Web Developer Career Lab Banner",
-    link: "https://preprlabs.org/labs/web-developer-career-lab"
+    link: "https://preprlabs.org/labs/web-developer-career-lab",
+    recommended: "yes"
   },
     {title: "UX Designer Career Lab",
     image: "https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/labs/01dWNBtgBXu2JjgCDBHySQhoh.png",
     imageAlt: "UX Designer Career Lab Banner",
-    link: "https://preprlabs.org/labs/ux-designer-career-lab"
+    link: "https://preprlabs.org/labs/ux-designer-career-lab",
+    recommended: "yes"
   },
     {title: "Project Manager Career Lab",
     image: "https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/labs/RRnJEtAB8PgMaFVfReVPXWbvn.png",
     imageAlt: "Project Manager Career Lab Banner",
-    link: "https://preprlabs.org/labs/project-manager-career-lab"
+    link: "https://preprlabs.org/labs/project-manager-career-lab",
+    recommended: "yes"
   }
   ]
 
@@ -61,22 +69,26 @@ function Home() {
     {title: "The Future of Finance",
     image: "https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/resources/image/9qhhqIMrnjCCblIUD2oBxSnE6uWoMMttqfK7Cm3G.png",
     imageAlt: "The Future of Finance Resource Banner",
-    link: "https://preprlabs.org/resource-module/the-future-of-finance/view"
+    link: "https://preprlabs.org/resource-module/the-future-of-finance/view",
+    recommended: "yes"
   },
     {title: "The Future of Hospitality",
     image: "https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/resources/image/ffOjSMBPNFHXOQCFNtqaby1RascSJrIDjm7so52x.png",
     imageAlt: "The Future of Hospitality Resource Banner",
-    link: "https://preprlabs.org/resource-module/the-future-of-hospitality/view"
+    link: "https://preprlabs.org/resource-module/the-future-of-hospitality/view",
+    recommended: "yes"
   },
     {title: "Identifying Future Life Trends In Transportation",
     image: "https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/resources/image/e85ZYA2owVaNwXAEGkrLH1Dgal5J7l4Ay89nMTTG.png",
     imageAlt: "The Future of Transportation Resource Banner",
-    link: "https://preprlabs.org/resource-module/identifying-future-life-trends-in-transportation/view"
+    link: "https://preprlabs.org/resource-module/identifying-future-life-trends-in-transportation/view",
+    recommended: "yes"
   },
     {title: "Global Methane Assessment - Benefits and Costs of Mitigating Methane Emissions",
     image: "https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/resources/image/GUGkVv7xXA1yYhCcJywOAiV3WBgyTjirQKqcfLNW.png",
     imageAlt: "Global Methane Assessment Resource Banner",
-    link: "https://preprlabs.org/resource-module/global-methane-assessment-benefits-and-costs-of-mitigating-methane-emissions/view"
+    link: "https://preprlabs.org/resource-module/global-methane-assessment-benefits-and-costs-of-mitigating-methane-emissions/view",
+    recommended: "yes"
   }
   ]
 
@@ -102,8 +114,10 @@ function Home() {
                   />
           ))}
         </CardGroup>
-
-        <Button className="exploreMoreButton text-right float-right" variant="outline-success">Explore More Challenges</Button>{' '}
+        
+        <div className="text-right">
+          <Button className="exploreMoreButton" variant="outline-success">Explore More Challenges</Button>{' '}
+        </div>
 
         <h1>Recommended Labs</h1>
 
@@ -118,7 +132,9 @@ function Home() {
           ))}
         </CardGroup>
 
-        <Button className="exploreMoreButton text-right float-right" variant="outline-success">Explore More Labs</Button>{' '}
+        <div className="text-right">
+          <Button className="exploreMoreButton" variant="outline-success">Explore More Labs</Button>{' '}
+        </div>
 
         <h1>Recommended Resources</h1>
 
@@ -133,8 +149,34 @@ function Home() {
           ))}
         </CardGroup>
 
-        <Button className="exploreMoreButton text-right float-right" variant="outline-success">Explore More Resources</Button>{' '}
+        <div className="text-right">
+          <Button className="exploreMoreButton" variant="outline-success">Explore More Resources</Button>{' '}
+        </div>
 
+      </div>
+      
+      <div className="explore">
+
+      <h1>Explore</h1>
+
+      <Tabs
+      id="controlled-tab-example"
+      activeKey={key}
+      onSelect={(k) => setKey(k)}
+      className="exploreTabs mb-3 text-center"
+    >
+        <Tab eventKey="challenges" title="Challenges">
+          {/* <Sonnet /> */}
+          <p>test test</p>
+        </Tab>
+        <Tab eventKey="labs" title="Labs">
+          <p>more tests</p>
+        </Tab>
+        <Tab eventKey="projects" title="Projects">
+          <p>even more tests</p>
+        </Tab>
+      </Tabs>
+      
       </div>
       
 
