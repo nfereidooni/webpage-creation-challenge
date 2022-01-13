b6be87b0a42f3fc54967572a40be9e2ec0c81594
Niki Fereidooni <nikifereidooni@gmail.com>
2022-01-13 16:23:46 -0500

    designed wide horizontal card using MD bootstrap

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,7 @@
 import { React, useState } from "react";
 import "./style.css";
-import { Container, Image, Card, CardGroup, Button, Tabs, Tab} from "react-bootstrap";
+import { Container, Image, CardGroup, Button, Tabs, Tab} from "react-bootstrap";
+
 import banner_01 from "../assets/img/preprbanner_01.jpg"
 import RecommendedCard from "../RecommendedCard"
 
@@ -15,6 +16,7 @@ function Home() {
     imageAlt: "Backend Developer Challenge icon Banner",
     link: "https://preprlabs.org/challengeManager/backend-developer-challenge-1-sql",
     recommended: "yes",
+    type: "Challenge",
     description: "Introduction: SQL is a query language that is used to interact with databases. It is a Domain Specific Language, meaning it is a specialized language that is used for working with data in databases."
   },
     {title: "Backend Developer Challenge 2: HTML, CSS and PHP",
@@ -22,6 +24,7 @@ function Home() {
     imageAlt: "Backend Developer Challenge icon Banner",
     link: "https://preprlabs.org/challengeManager/backend-developer-challenge-2-html-css-and-php",
     recommended: "yes",
+    type: "Challenge",
     description: "Introduction: HTML is the basis for all web pages on the internet and CSS dictates how HTML elements will be displayed. In this challenge you will be given a template for a basic web page you will also use PHP to create a search functionality for the website."
   },
     {title: "Backend Developer Challenge 3: Ruby On Rails",
@@ -29,6 +32,7 @@ function Home() {
     imageAlt: "Backend Developer Challenge Banner",
     link: "https://preprlabs.org/challengeManager/backend-developer-challenge-3-ruby-on-rails",
     recommended: "yes",
+    type: "Challenge",
     description: "Introduction: Ruby is an open-source object-oriented scripting language that is used in both front-end and back-end development to build web applications. It also has applications such as data analysis and prototyping."
   },
     {title: "Backend Developer Challenge 4: JavaScript",
@@ -36,6 +40,7 @@ function Home() {
     imageAlt: "Backend Developer Challenge Banner",
     link: "https://preprlabs.org/challengeManager/backend-developer-challenge-4-javascript",
     recommended: "yes",
+    type: "Challenge",
     description: "Introduction: JavaScript is an interpreted scripting language that is used to make web pages interactive and program webpage behaviour. JavaScript is run from inside HTML web pages, so it must be called from within the webpage to run."
   }
   ]
@@ -47,6 +52,7 @@ function Home() {
     imageAlt: "FUN Winter 2021 logo Banner",
     link: "https://preprlabs.org/labs/fun-lab-winter-2021",
     recommended: "yes",
+    type: "Lab",
     description: "Welcome to the Winter 2021 cohort of the F.U.N. program! We are so excited to help you launch your career over the next few months. This Lab is going to be your resource hub where we will be sharing resources and key updates. Check back regularly!"
   },
     {title: "Web Developer Career Lab",
@@ -54,6 +60,7 @@ function Home() {
     imageAlt: "Web Developer Career Lab Banner",
     link: "https://preprlabs.org/labs/web-developer-career-lab",
     recommended: "yes",
+    type: "Lab",
     description: ""
   },
     {title: "UX Designer Career Lab",
@@ -61,6 +68,7 @@ function Home() {
     imageAlt: "UX Designer Career Lab Banner",
     link: "https://preprlabs.org/labs/ux-designer-career-lab",
     recommended: "yes",
+    type: "Lab",
     description: ""
   },
     {title: "Project Manager Career Lab",
@@ -68,6 +76,7 @@ function Home() {
     imageAlt: "Project Manager Career Lab Banner",
     link: "https://preprlabs.org/labs/project-manager-career-lab",
     recommended: "yes",
+    type: "Lab",
     description: ""
   }
   ]
@@ -78,24 +87,28 @@ function Home() {
     image: "https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/resources/image/9qhhqIMrnjCCblIUD2oBxSnE6uWoMMttqfK7Cm3G.png",
     imageAlt: "The Future of Finance Resource Banner",
     link: "https://preprlabs.org/resource-module/the-future-of-finance/view",
+    type: "Resource",
     recommended: "yes"
   },
     {title: "The Future of Hospitality",
     image: "https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/resources/image/ffOjSMBPNFHXOQCFNtqaby1RascSJrIDjm7so52x.png",
     imageAlt: "The Future of Hospitality Resource Banner",
     link: "https://preprlabs.org/resource-module/the-future-of-hospitality/view",
+    type: "Resource",
     recommended: "yes"
   },
     {title: "Identifying Future Life Trends In Transportation",
     image: "https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/resources/image/e85ZYA2owVaNwXAEGkrLH1Dgal5J7l4Ay89nMTTG.png",
     imageAlt: "The Future of Transportation Resource Banner",
     link: "https://preprlabs.org/resource-module/identifying-future-life-trends-in-transportation/view",
+    type: "Resource",
     recommended: "yes"
   },
     {title: "Global Methane Assessment - Benefits and Costs of Mitigating Methane Emissions",
     image: "https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/resources/image/GUGkVv7xXA1yYhCcJywOAiV3WBgyTjirQKqcfLNW.png",
     imageAlt: "Global Methane Assessment Resource Banner",
     link: "https://preprlabs.org/resource-module/global-methane-assessment-benefits-and-costs-of-mitigating-methane-emissions/view",
+    type: "Resource",
     recommended: "yes"
   }
   ]
@@ -177,22 +190,8 @@ function Home() {
     >
         <Tab eventKey="challenges" title="Challenges">
           {/* <Sonnet /> */}
-          <p>test test</p>
-          <CardGroup>
-            <Card className="wideCard" style={{ maxWidth: '540px'}}>
-              <div className="row g-0">
-                <div className="col-4">
-                  <Card.Img variant="right" src="https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/challenge/hn6fd9vCImNkx6Eygm4cNkUnO.png" alt="Backend Developer Challenge icon Banner" />
-                </div>
-                <div className="col-8">
-                  <Card.Body>
-                    <Card.Link href="https://preprlabs.org/challengeManager/backend-developer-challenge-1-sql">Backend Developer Challenge 1: SQL</Card.Link>
-                    <Card.Text>Introduction: SQL is a query language that is used to interact with databases. It is a Domain Specific Language, meaning it is a specialized language that is used for working with data in databases.</Card.Text>
-                  </Card.Body>
-                </div>
-              </div> 
-            </Card>
-          </CardGroup>
+
+
         </Tab>
         <Tab eventKey="labs" title="Labs">
           <p>more tests</p>
--- src/components/WideCard/index.js
+++ src/components/WideCard/index.js
@@ -1,18 +1,30 @@
 import React  from "react"
 import "./style.css";
-import { Card } from "react-bootstrap";
+import { MDBCard, MDBCardLink, MDBCardText, MDBCardBody, MDBCardImage, MDBRow, MDBCol } from 'mdb-react-ui-kit'
 
 
 function WideCard(props) {
 
   return(
     <>          
-          <Card className="wideCard">
-            <Card.Img variant="right" src={props.image} alt={props.imageAlt} />
-            <Card.Body>
-            <Card.Link href={props.link}>{props.title}</Card.Link>
-            </Card.Body>
-          </Card>
+          <MDBCard>
+            <MDBRow className='g-0'>
+              <MDBCol md='4'>
+                <MDBCardImage src='https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/challenge/hn6fd9vCImNkx6Eygm4cNkUnO.png' alt='Backend Developer Challenge icon Banner' fluid />
+              </MDBCol>
+              <MDBCol md='8'>
+                <MDBCardBody>
+                  <MDBCardLink href="https://preprlabs.org/challengeManager/backend-developer-challenge-1-sql">Backend Developer Challenge 1: SQL</MDBCardLink>
+                  <MDBCardText>
+                    <small className='text-muted'>Challenge</small>
+                  </MDBCardText>
+                  <MDBCardText>
+                  Introduction: SQL is a query language that is used to interact with databases. It is a Domain Specific Language, meaning it is a specialized language that is used for working with data in databases.
+                  </MDBCardText>
+                </MDBCardBody>
+              </MDBCol>
+            </MDBRow>
+          </MDBCard>
   </>
   );
 }
