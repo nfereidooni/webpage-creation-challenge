794ec927c5e0f34822c46340858789ac096bc441
Niki Fereidooni <nikifereidooni@gmail.com>
2022-01-13 16:30:38 -0500

    running props through new wide card now

--- src/components/WideCard/index.js
+++ src/components/WideCard/index.js
@@ -10,16 +10,16 @@ function WideCard(props) {
           <MDBCard>
             <MDBRow className='g-0'>
               <MDBCol md='4'>
-                <MDBCardImage src='https://prepr-media-live.s3.ca-central-1.amazonaws.com/uploads/challenge/hn6fd9vCImNkx6Eygm4cNkUnO.png' alt='Backend Developer Challenge icon Banner' fluid />
+                <MDBCardImage src={props.image} alt={props.imageAlt} fluid />
               </MDBCol>
               <MDBCol md='8'>
                 <MDBCardBody>
-                  <MDBCardLink href="https://preprlabs.org/challengeManager/backend-developer-challenge-1-sql">Backend Developer Challenge 1: SQL</MDBCardLink>
+                  <MDBCardLink href={props.link}>{props.title}</MDBCardLink>
                   <MDBCardText>
-                    <small className='text-muted'>Challenge</small>
+                    <small className='text-muted'>{props.type}</small>
                   </MDBCardText>
                   <MDBCardText>
-                  Introduction: SQL is a query language that is used to interact with databases. It is a Domain Specific Language, meaning it is a specialized language that is used for working with data in databases.
+                  {props.description}
                   </MDBCardText>
                 </MDBCardBody>
               </MDBCol>
