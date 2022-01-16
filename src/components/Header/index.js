ed1c5993822c036b0e1e2c23eeae4e774f5a8d09
Niki Fereidooni <nikifereidooni@gmail.com>
2022-01-16 15:25:29 -0500

    fixed one of the navbar bugs and started to work on App.scss file

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -27,45 +27,45 @@ function Header(props) {
                 <Image className="logo" src="https://d2qgo50yy98y8.cloudfront.net/public/front/img/logo.png"/>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ml-auto mt-1 mt-lg-0">
-                <NavDropdown className="dashboardLink" id="collaspible-nav-dropdown" title={<div style={{display: "inline-block"}}><i class="fas fa-binoculars"></i>&nbsp;Explore</div>}>
-                    <NavDropdown.Item href="#"><i class="fas fa-flask"></i>&nbsp;&nbsp;Labs</NavDropdown.Item>
-                    <NavDropdown.Item href="#"><i class="fas fa-puzzle-piece"></i>&nbsp;&nbsp;Challenges</NavDropdown.Item>
-                    <NavDropdown.Item href="#"><i class="fas fa-briefcase"></i>&nbsp;&nbsp;Organizations</NavDropdown.Item>
-                    <NavDropdown.Item href="#"><i class="fas fa-journal-whills"></i>&nbsp;&nbsp;Resources</NavDropdown.Item>
-                </NavDropdown>              
-                <NavDropdown className="dashboardLink" id="collapsible-nav-dropdown" title={<div style={{display: "inline-block"}}><i class="fas fa-tachometer-alt"></i> My Dashboard </div>}>
-                    <NavDropdown.Item href="#"><i class="fas fa-user"></i>&nbsp;&nbsp;User Dashboard</NavDropdown.Item>
-                    <NavDropdown.Item href="#"><i class="fas fa-project-diagram"></i>&nbsp;&nbsp;Projects</NavDropdown.Item>
-                    <NavDropdown.Item href="#"><i class="fas fa-user-friends"></i>&nbsp;&nbsp;Team Matching</NavDropdown.Item>
-                    <NavDropdown.Item href="#"><i class="fas fa-medal"></i>&nbsp;&nbsp;Achievements</NavDropdown.Item>
+            <Navbar.Collapse id="responsive-navbar-nav justify-content-end">
+              <Nav className="me-auto mt-1 mt-lg-0">
+                  <NavDropdown className="dashboardLink" id="collaspible-nav-dropdown" title={<div style={{display: "inline-block"}}><i class="fas fa-binoculars"></i>&nbsp;Explore</div>}>
+                      <NavDropdown.Item href="#"><i class="fas fa-flask"></i>&nbsp;&nbsp;Labs</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><i class="fas fa-puzzle-piece"></i>&nbsp;&nbsp;Challenges</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><i class="fas fa-briefcase"></i>&nbsp;&nbsp;Organizations</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><i class="fas fa-journal-whills"></i>&nbsp;&nbsp;Resources</NavDropdown.Item>
+                  </NavDropdown>              
+                  <NavDropdown className="dashboardLink" id="collapsible-nav-dropdown" title={<div style={{display: "inline-block"}}><i class="fas fa-tachometer-alt"></i> My Dashboard </div>}>
+                      <NavDropdown.Item href="#"><i class="fas fa-user"></i>&nbsp;&nbsp;User Dashboard</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><i class="fas fa-project-diagram"></i>&nbsp;&nbsp;Projects</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><i class="fas fa-user-friends"></i>&nbsp;&nbsp;Team Matching</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><i class="fas fa-medal"></i>&nbsp;&nbsp;Achievements</NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown className="dashboardLink" id="collapsible-nav-dropdown" title={<div style={{display: "inline-block"}}><i class="fas fa-cog"></i> Manage </div>} >
+                      <NavDropdown.Item href="#"><i class="fas fa-user"></i>&nbsp;&nbsp;Profile</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><i class="fas fa-cog"></i>&nbsp;&nbsp;Settings</NavDropdown.Item>
+                  </NavDropdown>
+              </Nav>
+              <Form className="searchBar d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search Challenges, Labs, Projects and People"
+                  className="form-control"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success"><i class="fa fa-search"></i></Button>
+              </Form>
+              <Nav className="mt-1 mt-lg-0">
+                  <NavDropdown className="userMenu" id="collapsible-nav-dropdown" title={<div style={{display: "inline-block"}}><Image src={userImage} width="40" roundedCircle/></div>}>
+                      <NavDropdown.Item className="userMenuItem" href="#"><i class="fas fa-user"></i>&nbsp;&nbsp;My Info</NavDropdown.Item>
+                      <NavDropdown.Item className="userMenuItem" href="#"><i class="fas fa-question-circle"></i>&nbsp;&nbsp;Help</NavDropdown.Item>
+                      <NavDropdown.Item className="userMenuItem" href="#"><i class="fas fa-inbox"></i>&nbsp;&nbsp;Inbox</NavDropdown.Item>
+                      <NavDropdown.Item className="userMenuItem" href="#"><i class="far fa-newspaper"></i>&nbsp;&nbsp;Newsfeed</NavDropdown.Item>
+                      <NavDropdown.Item className="userMenuItem" href="#"><i class="far fa-bell"></i>&nbsp;&nbsp;Notifications</NavDropdown.Item>
+                      <NavDropdown.Item className="userMenuItem" href="#"><i class="fas fa-cog"></i>&nbsp;&nbsp;Settings</NavDropdown.Item>
+                      <NavDropdown.Item className="userMenuItem" href="#">Logout</NavDropdown.Item>
                 </NavDropdown>
-                <NavDropdown className="dashboardLink" id="collapsible-nav-dropdown" title={<div style={{display: "inline-block"}}><i class="fas fa-cog"></i> Manage </div>} >
-                    <NavDropdown.Item href="#"><i class="fas fa-user"></i>&nbsp;&nbsp;Profile</NavDropdown.Item>
-                    <NavDropdown.Item href="#"><i class="fas fa-cog"></i>&nbsp;&nbsp;Settings</NavDropdown.Item>
-                </NavDropdown>
-            </Nav>
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                placeholder="Search Challenges, Labs, Projects and People"
-                className="form-control me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success"><i class="fa fa-search"></i></Button>
-            </Form>
-            <Nav className="ml-1 mt-1 pl-lg-5 mt-lg-0">
-                <NavDropdown className="userMenu" id="collapsible-nav-dropdown" title={<div style={{display: "inline-block"}}><Image src={userImage} width="40" roundedCircle/></div>}>
-                    <NavDropdown.Item className="userMenuItem" href="#"><i class="fas fa-user"></i>&nbsp;&nbsp;My Info</NavDropdown.Item>
-                    <NavDropdown.Item className="userMenuItem" href="#"><i class="fas fa-question-circle"></i>&nbsp;&nbsp;Help</NavDropdown.Item>
-                    <NavDropdown.Item className="userMenuItem" href="#"><i class="fas fa-inbox"></i>&nbsp;&nbsp;Inbox</NavDropdown.Item>
-                    <NavDropdown.Item className="userMenuItem" href="#"><i class="far fa-newspaper"></i>&nbsp;&nbsp;Newsfeed</NavDropdown.Item>
-                    <NavDropdown.Item className="userMenuItem" href="#"><i class="far fa-bell"></i>&nbsp;&nbsp;Notifications</NavDropdown.Item>
-                    <NavDropdown.Item className="userMenuItem" href="#"><i class="fas fa-cog"></i>&nbsp;&nbsp;Settings</NavDropdown.Item>
-                    <NavDropdown.Item className="userMenuItem" href="#">Logout</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>       
+              </Nav>       
             </Navbar.Collapse>
 
           </Container>
