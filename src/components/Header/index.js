9d86cc440e890b26c3128aa576da3b46e596c0fb
Niki Fereidooni <nikifereidooni@gmail.com>
2022-01-16 23:13:56 -0500

    fixed issue with text not lining up in the navbar dropdowns

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -28,20 +28,20 @@ function Header(props) {
             <Navbar.Collapse id="responsive-navbar-nav justify-content-end">
               <Nav className="me-auto mt-1 mt-lg-0">
                   <NavDropdown className="dashboardLink" id="collaspible-nav-dropdown" title={<div style={{display: "inline-block"}}><i class="fas fa-binoculars"></i>&nbsp;Explore</div>}>
-                      <NavDropdown.Item href="#"><i class="fas fa-flask"></i>&nbsp;&nbsp;Labs</NavDropdown.Item>
-                      <NavDropdown.Item href="#"><i class="fas fa-puzzle-piece"></i>&nbsp;&nbsp;Challenges</NavDropdown.Item>
-                      <NavDropdown.Item href="#"><i class="fas fa-briefcase"></i>&nbsp;&nbsp;Organizations</NavDropdown.Item>
-                      <NavDropdown.Item href="#"><i class="fas fa-journal-whills"></i>&nbsp;&nbsp;Resources</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><span className="icon"><i class="fas fa-flask"></i></span>Labs</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><span className="icon"><i class="fas fa-puzzle-piece"></i></span>Challenges</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><span className="icon"><i class="fas fa-briefcase"></i></span>Organizations</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><span className="icon"><i class="fas fa-journal-whills"></i></span>Resources</NavDropdown.Item>
                   </NavDropdown>              
                   <NavDropdown className="dashboardLink" id="collapsible-nav-dropdown" title={<div style={{display: "inline-block"}}><i class="fas fa-tachometer-alt"></i> My Dashboard </div>}>
-                      <NavDropdown.Item href="#"><i class="fas fa-user"></i>&nbsp;&nbsp;User Dashboard</NavDropdown.Item>
-                      <NavDropdown.Item href="#"><i class="fas fa-project-diagram"></i>&nbsp;&nbsp;Projects</NavDropdown.Item>
-                      <NavDropdown.Item href="#"><i class="fas fa-user-friends"></i>&nbsp;&nbsp;Team Matching</NavDropdown.Item>
-                      <NavDropdown.Item href="#"><i class="fas fa-medal"></i>&nbsp;&nbsp;Achievements</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><span className="icon"><i class="fas fa-user"></i></span>User Dashboard</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><span className="icon"><i class="fas fa-project-diagram"></i></span>Projects</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><span className="icon"><i class="fas fa-user-friends"></i></span>Team Matching</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><span className="icon"><i class="fas fa-medal"></i></span>Achievements</NavDropdown.Item>
                   </NavDropdown>
                   <NavDropdown className="dashboardLink" id="collapsible-nav-dropdown" title={<div style={{display: "inline-block"}}><i class="fas fa-cog"></i> Manage </div>} >
-                      <NavDropdown.Item href="#"><i class="fas fa-user"></i>&nbsp;&nbsp;Profile</NavDropdown.Item>
-                      <NavDropdown.Item href="#"><i class="fas fa-cog"></i>&nbsp;&nbsp;Settings</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><span className="icon"><i class="fas fa-user"></i></span>Profile</NavDropdown.Item>
+                      <NavDropdown.Item href="#"><span className="icon"><i class="fas fa-cog"></i></span>Settings</NavDropdown.Item>
                   </NavDropdown>
               </Nav>
               <Form className="searchBar d-flex">
@@ -55,12 +55,12 @@ function Header(props) {
               </Form>
               <Nav className="mt-1 mt-lg-0">
                   <NavDropdown className="userMenu" id="collapsible-nav-dropdown" title={<div style={{display: "inline-block"}}><Image src={userImage} width="40" roundedCircle/></div>}>
-                      <NavDropdown.Item className="userMenuItem" href="#"><i class="fas fa-user"></i>&nbsp;&nbsp;My Info</NavDropdown.Item>
-                      <NavDropdown.Item className="userMenuItem" href="#"><i class="fas fa-question-circle"></i>&nbsp;&nbsp;Help</NavDropdown.Item>
-                      <NavDropdown.Item className="userMenuItem" href="#"><i class="fas fa-inbox"></i>&nbsp;&nbsp;Inbox</NavDropdown.Item>
-                      <NavDropdown.Item className="userMenuItem" href="#"><i class="far fa-newspaper"></i>&nbsp;&nbsp;Newsfeed</NavDropdown.Item>
-                      <NavDropdown.Item className="userMenuItem" href="#"><i class="far fa-bell"></i>&nbsp;&nbsp;Notifications</NavDropdown.Item>
-                      <NavDropdown.Item className="userMenuItem" href="#"><i class="fas fa-cog"></i>&nbsp;&nbsp;Settings</NavDropdown.Item>
+                      <NavDropdown.Item className="userMenuItem" href="#"><span className="icon"><i class="fas fa-user"></i></span>My Info</NavDropdown.Item>
+                      <NavDropdown.Item className="userMenuItem" href="#"><span className="icon"><i class="fas fa-question-circle"></i></span>Help</NavDropdown.Item>
+                      <NavDropdown.Item className="userMenuItem" href="#"><span className="icon"><i class="fas fa-inbox"></i></span>Inbox</NavDropdown.Item>
+                      <NavDropdown.Item className="userMenuItem" href="#"><span className="icon"><i class="far fa-newspaper"></i></span>Newsfeed</NavDropdown.Item>
+                      <NavDropdown.Item className="userMenuItem" href="#"><span className="icon"><i class="far fa-bell"></i></span>Notifications</NavDropdown.Item>
+                      <NavDropdown.Item className="userMenuItem" href="#"><span className="icon"><i class="fas fa-cog"></i></span>Settings</NavDropdown.Item>
                       <NavDropdown.Item className="userMenuItem" href="#">Logout</NavDropdown.Item>
                 </NavDropdown>
               </Nav>       
